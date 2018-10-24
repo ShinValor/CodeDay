3be86af25670f51c4e07579a7a41067e77f30a80
Shinvalor <hpoon16@huntersoe.org>
2018-10-24 02:48:59 -0400

    views not connected

--- backendCode/controllers/index.js
+++ backendCode/controllers/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.use('/alt', require('./alt'));
+//router.use('/alt', require('./alt'));
 router.use('/', require('./home'));
 
 
